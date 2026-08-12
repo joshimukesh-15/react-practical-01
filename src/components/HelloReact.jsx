56b749a339d964e9186362b441708c3a993fb596
Mukesh Joshi <joshimukesh2005@gmail.com>
2026-08-12 16:56:58 +0530

    Initial commit for Practical 01

---
+++ src/components/HelloReact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HelloReact() {
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <h2>Welcome to React JS Development</h2>
+      <p>This is my first React application.</p>
+    </div>
+  );
+}
+
+export default HelloReact;
